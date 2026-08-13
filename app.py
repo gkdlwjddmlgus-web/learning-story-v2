87152468af926d8062f4e0e90e2c14a800962c2a
gkdlwjddmlgus-web <gkdlwjddmlgus@gmail.com>
2026-08-13 18:21:42 +0900

    Initialize Learning Story v2

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Learning Story",
+    page_icon="📖",
+    layout="wide",
+)
+
+st.title("Learning Story")
+st.write("공부할수록 당신의 이야기가 진행됩니다.")
